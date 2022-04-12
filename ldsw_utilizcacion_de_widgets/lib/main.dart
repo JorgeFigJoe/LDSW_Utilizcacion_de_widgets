31554a6d7cabb25f9d7097e1301fba936c629a5a
Jorge Figueroa <50466247+JorgeFigJoe@users.noreply.github.com>
2022-04-12 00:34:50 -0500

    add gridView

--- ldsw_utilizcacion_de_widgets/lib/main.dart
+++ ldsw_utilizcacion_de_widgets/lib/main.dart
@@ -1,35 +1,70 @@
+import 'dart:convert';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:ldsw_utilizcacion_de_widgets/columns.dart';
-import 'package:ldsw_utilizcacion_de_widgets/rows.dart';
-import 'package:ldsw_utilizcacion_de_widgets/stack.dart';
+import 'package:http/http.dart' as http;
 
-void main() {
-  runApp(EjemplosWidgets());
+
+void main() => runApp(MaterialApp(
+  title: "Poke App",
+  home: HomeScreen(),
+  debugShowCheckedModeBanner: false,
+));
+
+class HomeScreen extends StatefulWidget{
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
-class EjemplosWidgets extends StatelessWidget {
+class _HomeScreenState extends State<HomeScreen>{
+  var pokeApi = "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json";
+  late List pokedex;
 
   @override
-  Widget build(BuildContext){
-    return MaterialApp(
-      home: Scaffold(
-        body: Container(
-          width: double.infinity,
-          color: Colors.black,
-          child: Center(
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                Columns(),
-                Rowss(),
-                Stacks()
-              ],
-            ),
-          ),
-        ),
-      ),
+  void initState() {
+    super.initState();
+    if(mounted){
+      fetchPokemonData();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Column(
+        children: [
+         pokedex != null ? Expanded(child: GridView.builder(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount:
+          2, childAspectRatio: 1.4,
+          ), itemCount: pokedex.length,
+          itemBuilder: (context, index){
+            return Card(
+              child: Column(
+                children: [
+                  Text(
+                      pokedex[index]['name']
+                  ),
+                  CachedNetworkImage(imageUrl: pokedex[index]['img'])
+                ],
+              )
+            );
+          },
+          )) : Center(
+           child: CircularProgressIndicator()
+         )
+        ],
+      )
     );
   }
 
-}
+  void fetchPokemonData(){
+    var url = Uri.https("raw.githubusercontent.com", "/Biuni/PokemonGO-Pokedex/master/pokedex.json");
+    http.get(url).then((value){
+      var decodeJson = jsonDecode(value.body);
+      //print(decodeJson);
+      pokedex = decodeJson['pokemon'];
+      print(pokedex);
+      setState(() {
 
+      });
+    });
+  }
+}
